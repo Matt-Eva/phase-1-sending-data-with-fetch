94c62765bb2369307b6fbf8a333917f6b8106b3b
Matt Eva <matteweva@gmail.com>
2021-10-22 15:50:27 -0700

    commit tree 140c09534fbffe6365004d5381f546e3ac0f5c1b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+const body = document.querySelector('body')
+
+function submitData(userName, userEmail){
+const userInfo = {
+    name: userName,
+    email: userEmail,
+}
+
+const configurtationObj = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(userInfo)
+}
+
+
+
+return fetch('http://localhost:3000/users', configurtationObj)
+.then(res => res.json())
+.then(data =>{
+body.append(data.id);
+}).catch( error => {
+    body.append(error.message)
+})
+}
+
+// console.log(submitData('Billy', 'gmail'));
